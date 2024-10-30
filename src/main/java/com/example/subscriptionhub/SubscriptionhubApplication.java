45586f03db07c72db73b4d719bca873a1d91a6d1
William Kågebo <william.kagebo@gmail.com>
2024-10-30 21:34:44 +0100

    Initial commit
    Contains the initial config from the Spring Initializr

---
+++ src/main/java/com/example/subscriptionhub/SubscriptionhubApplication.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionhubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/subscriptionhub/SubscriptionhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subscriptionhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
